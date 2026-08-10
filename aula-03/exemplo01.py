8b87306ea696b0420cad1c1e6c7f5781f32b08bc
gbauerg <gabriel.bauer2018@outlook.com>
2026-08-10 20:41:38 -0300

    Atividade feita em sala de aula.

--- aula-03/exemplo01.py
+++ aula-03/exemplo01.py
@@ -1,2 +1,15 @@
-#github
-print("Github - Aula 03")
+#Veiculo percorre 10km p/ litro (pensar duas distancise definir combustivel necessário)
+
+distancia1 = float(input("Qual a primeira distância em Kilometros? "))
+distancia2 = float(input("Qual a segunda distância em Kilometros? "))
+
+somadistancia = distancia1 + distancia2
+
+AUTONOMIA = 10 #Constante em upcase
+
+consumocombustivel1 = distancia1/AUTONOMIA
+consumocombustivel2 = distancia2/AUTONOMIA
+consumototal = somadistancia/AUTONOMIA
+
+print(f"A distância percorrida foi {somadistancia}Km.")
+print(f"O consumo no primeiro trecho foi {consumocombustivel1}L. No segundo trecho, {consumocombustivel2}L. O consumo total foi {consumototal}L")
